c1e2742c136579ace5c039e7a2ba053960f655c1
Abhayanth K <abhayanth2006@gmail.com>
2025-09-22 17:45:18 +0530

    Specs_Section_Added

--- app/(public)/page.jsx
+++ app/(public)/page.jsx
@@ -3,12 +3,15 @@ import React from "react";
 import Hero from "@/components/Hero";
 import LatestProducts from "@/components/LatestProducts";
 import BestSelling from "@/components/BestSelling";
+import OurSpecs from "@/components/OurSpec";
+
 export default function Home(){
       return (
             <div>
                   <Hero />
                   <LatestProducts />
                   <BestSelling />
+                  <OurSpecs />
             </div>
       )
 }
